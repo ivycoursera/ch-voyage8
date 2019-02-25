feb2e4e80fb567d9e8c1921710473e5aca6530d9
Vi <liliput_j@yahoo.com>
2019-02-25 09:56:57 -0800

    Separated SearchBar into its own component

--- bookbinder/src/App.js
+++ bookbinder/src/App.js
@@ -1,50 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import InputGroup from 'react-bootstrap/InputGroup';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
+import SearchBar from './SearchBar';
 
 class App extends Component {
-  state = {
-    query: ""
-  }
-
-  handleChange(query) {
-   this.setState({
-      query: query
-    })
-  }
-
-  handleClick(){
-    this.setState({
-      query: ""
-    })
-  }
 
   render() {
-    const isVisible = this.state.query!=="";
     return (
       <div className="App">
         <header className="App-header">
           <h1>BOOK FINDER</h1>
         </header>
-        <main>
-          <InputGroup>
-            <FormControl
-              placeholder= "Search by book name"
-              onChange={(e) => this.handleChange(e.target.value)}
-              value= {this.state.query}
-            />
-            <InputGroup.Append>
-              <Button
-                variant="light"
-                onClick ={() => this.handleClick()}
-                hidden = {!isVisible}
-              >X</Button>
-              <Button disabled>Search</Button>
-            </InputGroup.Append>
-          </InputGroup>
-        </main>
+        <SearchBar />
       </div>
     );
   }
---
+++ bookbinder/src/SearchBar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+
+class SearchBar extends Component {
+    state = {
+      query: ""
+    }
+
+    handleChange(query) {
+     this.setState({
+        query: query
+      })
+    }
+
+    handleClick(){
+      this.setState({
+        query: ""
+      })
+    }
+
+    render() {
+      const isVisible = this.state.query!=="";
+      return (
+        <main>
+            <InputGroup>
+              <FormControl
+                placeholder= "Search by book name"
+                onChange={(e) => this.handleChange(e.target.value)}
+                value= {this.state.query}
+              />
+              <InputGroup.Append>
+                <Button
+                  variant="light"
+                  onClick ={() => this.handleClick()}
+                  hidden = {!isVisible}
+                >X</Button>
+                <Button disabled>Search</Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </main>
+      );
+    }
+  }
+
+  export default SearchBar;
