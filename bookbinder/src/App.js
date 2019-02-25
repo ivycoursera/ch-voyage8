a87e627efc922821e05f3ebcdc6eb9994654a08a
Vi <liliput_j@yahoo.com>
2019-02-25 09:46:11 -0800

    Modified form to use react bootstrap

--- bookbinder/src/App.js
+++ bookbinder/src/App.js
@@ -1,24 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 
 class App extends Component {
+  state = {
+    query: ""
+  }
+
+  handleChange(query) {
+   this.setState({
+      query: query
+    })
+  }
+
+  handleClick(){
+    this.setState({
+      query: ""
+    })
+  }
+
   render() {
+    const isVisible = this.state.query!=="";
     return (
       <div className="App">
         <header className="App-header">
           <h1>BOOK FINDER</h1>
         </header>
         <main>
-          <div className="search-bar">
-            <form>
-              <input type='text' className="search-input" name="books" />
-              <button className="hide">X</button>
-              <input type="submit" className="search-submit" value="Search"/>
-            </form>
-          </div>
-          <div className="book-list">
-              <p>Nothing to show yet. Try searching for a book.</p>
-          </div>
+          <InputGroup>
+            <FormControl
+              placeholder= "Search by book name"
+              onChange={(e) => this.handleChange(e.target.value)}
+              value= {this.state.query}
+            />
+            <InputGroup.Append>
+              <Button
+                variant="light"
+                onClick ={() => this.handleClick()}
+                hidden = {!isVisible}
+              >X</Button>
+              <Button disabled>Search</Button>
+            </InputGroup.Append>
+          </InputGroup>
         </main>
       </div>
     );
